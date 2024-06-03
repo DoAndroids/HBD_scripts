093830456af02c61d2e009eb7a8aeffd9eebf9a0
GitHubMilo <git@milesroper.com>
2024-06-03 19:24:41 +0100

    Update torrentInspector.sh

--- torrentInspector.sh
+++ torrentInspector.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+
 basePath="$(dirname "$(readlink -f "$0")")"
+
 if [ ! -d "${basePath}/.venv" ]; then
     python3 -m virtualenv "${basePath}/.venv"
 fi
+
+if [ ! -f "${basePath}/.env" ]; then
+    echo "You need to create a .env file in the ${basePath} directory"
+else
+    if ! grep -q "qbitUser" "${basePath}/.env"; then
+        echo "Your .env file needs to contain the \"qbitUser\" variable"
+        exit
+    fi
+    if ! grep -q "qbitPass" "${basePath}/.env"; then
+        echo "Your .env file needs to contain the \"qbitPass\" variable"
+        exit
+    fi
+    if ! grep -q "hostIp" "${basePath}/.env"; then
+        echo "Your .env file needs to contain the \"hostIp\" variable"
+        exit
+    fi
+fi
+
 if [ -d "${basePath}/.venv" ]; then
     source ${basePath}/.venv/bin/activate
     pip3 install -q --upgrade pip setuptools wheel
